6fb10054dbc97c9777914a338a1146f3f7bb713e
MAngelesGB <mgutierrez6@ucol.mx>
2023-07-29 01:16:02 -0600

    Estructura y color de la pagina

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 let app = document.getElementById('typewriter');
- 
+
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
 });
- 
+
 typewriter
   .pauseFor(2500)
-  .typeString('Desarrollo sitios web y escribo artículos sobre programación.')
+  .typeString('Desarrollo sitios web. Back-end y Front-end')
   .pauseFor(200)
   .deleteChars(10)
   .start();
